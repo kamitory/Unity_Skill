b7db286eb6e120a5e770a225f7085ca5e1a03ec6
kamitory <neco.hoosen@gmail.com>
2020-06-14 03:13:55 +0900

    10장 유도미사일

---
+++ Unity_Skill/Assets/_KMB/Scripts/AutoArrow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoArrow : MonoBehaviour
+{
+    [SerializeField] GameObject m_goArrow = null;
+    [SerializeField] Transform m_tfArrowSpawn = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject t_arrow = Instantiate(m_goArrow, m_tfArrowSpawn.position, Quaternion.identity);
+            t_arrow.GetComponent<Rigidbody>().velocity = Vector3.up * 5f;
+        }
+        
+    }
+}
