ddaafa9f75e475f89931d57c6bbfd21ad77b2159
kamitory <neco.hoosen@gmail.com>
2020-06-13 18:06:47 +0900

    Keidy Unity Skill 1장,2장

---
+++ Unity_Skill/Assets/Scripts/CreateCube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube : MonoBehaviour
+{
+    //public GameObject m_goPrefab = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CreateCoroutine());
+    }
+
+    // Update is called once per frame
+    IEnumerator CreateCoroutine()
+    {
+        while (true)
+        {
+            yield return null;
+            GameObject t_object = ObjectPoolingManager.instance.GetQueue();
+            t_object.transform.position = Vector3.zero;
+        }
+    }
+}
